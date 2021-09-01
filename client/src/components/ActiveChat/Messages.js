67eb0c84c1be4db92305f4e452fa492c65c86653
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-08-31 23:12:18 -0400

    styled live chat to scroll conversations only and auto scroll to bottom

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  chatBox: {
+    maxHeight: "85%",
+    overflow: "auto",
+    padding: "2%",
+  },
+}));
 
 const Messages = (props) => {
+  const classes = useStyles();
   const { messages, otherUser, userId } = props;
 
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    }
+  });
+
   return (
-    <Box>
+    <Box className={classes.chatBox}>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
+      <div ref={scrollRef} />
     </Box>
   );
 };
