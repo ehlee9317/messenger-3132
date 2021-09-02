c14f34495414d287bfa5b62f140cda96af3f344c
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-09-02 13:49:37 -0400

    Changed styles for login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,51 @@
 import React from "react";
-import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import {
-  Grid,
-  Box,
-  Typography,
-  Button,
-  FormControl,
-  TextField,
-} from "@material-ui/core";
+import { Redirect } from "react-router-dom";
+import { Grid, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { login } from "./store/utils/thunkCreators";
+import { theme } from "./themes/theme";
+import AuthSideLayout from "./components/AuthPage/AuthSideLayout";
+import AuthInput from "./components/AuthPage/AuthInput";
+import AuthHeader from "./components/AuthPage/AuthHeader";
+
+const useStyles = makeStyles(() => ({
+  formContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  form: {
+    flex: "0 500px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    marginTop: "100px",
+  },
+
+  greeting: {
+    fontSize: "26pt",
+    fontWeight: 600,
+    lineHeight: "40px",
+    marginBottom: "40px",
+  },
+
+  button: {
+    alignSelf: "center",
+    fontSize: "14pt",
+    fontWeight: "bold",
+    lineHeight: "20px",
+    padding: "20px 65px",
+    backgroundColor: theme.palette.primary.main,
+    color: "white",
+    marginTop: "30px",
+  },
+}));
 
 const Login = (props) => {
-  const history = useHistory();
   const { user, login } = props;
+  const classes = useStyles();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -28,41 +60,27 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
+    <AuthSideLayout>
+      <AuthHeader
+        text="Don't have an account?"
+        button="Create account"
+        link="register"
+      />
+
+      <Grid className={classes.formContainer}>
+        <form className={classes.form} onSubmit={handleLogin}>
+          <Typography className={classes.greeting}>Welcome back!</Typography>
+
+          <AuthInput label="Username" name="username" type="text" />
+
+          <AuthInput label="Password" name="password" type="password" />
+
+          <Button className={classes.button} type="submit" size="large">
+            Login
+          </Button>
         </form>
-      </Box>
-    </Grid>
+      </Grid>
+    </AuthSideLayout>
   );
 };
 
